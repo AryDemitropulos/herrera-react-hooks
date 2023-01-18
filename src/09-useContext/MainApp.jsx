0d3ed55e8ae58a3d4687cde1ac88bb79f153e72c
Ary <demitropulosary@gmail.com>
2023-01-17 22:26:36 -0300

    Se agrega CartContext

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,20 +1,25 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+import { CartProvider } from './context/CartContext';
 import { UserProvider } from './context/UserProvider';
 import { AboutPage, HomePage, LoginPage } from './pages';
+import { CartPage } from './pages/CartPage';
 
 export const MainApp = () => {
   return (
-    <UserProvider>
-      <h1>MainApp</h1>
-      <Navbar />
-      <hr />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/*' element={<Navigate to='/about' />} />
-      </Routes>
-    </UserProvider>
+    <CartProvider>
+      <UserProvider>
+        <h1>MainApp</h1>
+        <Navbar />
+        <hr />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/cart' element={<CartPage />} />
+          <Route path='/*' element={<Navigate to='/about' />} />
+        </Routes>
+      </UserProvider>
+    </CartProvider>
   );
 };
